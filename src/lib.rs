204e80db846d9accf4c44aebce8f6e9a29e6fd51
Sergey Shandar <sergey.shandar@gmail.com>
2019-01-31 07:14:14 -0800

    0.4.0 only chain references

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,30 @@
-pub struct ChainIntoIter<A, B>
-    where
-        A: IntoIterator + Copy,
-        B: IntoIterator<Item = A::Item> + Copy,
-{
+pub struct ChainIntoIter<
+    A: IntoIterator + Copy,
+    B: IntoIterator<Item = A::Item> + Copy
+> {
     a: A,
     b: B,
 }
 
-pub trait Chain
-    where Self: IntoIterator + Copy
-{
-    fn chain<B>(self, b: B) -> ChainIntoIter<Self, B>
-        where B: IntoIterator<Item = Self::Item> + Copy;
-}
-
-impl<T> Chain for T
-    where T: IntoIterator + Copy
-{
-    fn chain<B>(self, b: B) -> ChainIntoIter<Self, B>
-        where B: IntoIterator<Item = Self::Item> + Copy
-    {
+pub trait Chain: IntoIterator + Copy {
+    /// ```
+    /// use chain_intoiter::Chain;
+    /// let result = [1, 2, 3].chain(&[4, 5, 6]);
+    /// let x = result.chain(&[7, 8, 9]);
+    /// let v: Vec<isize> = x.into_iter().map(|x| *x).collect();
+    /// assert_eq!(vec!(1, 2, 3, 4, 5, 6, 7, 8, 9), v);
+    /// ```
+    fn chain<B: IntoIterator<Item = Self::Item> + Copy>(self, b: B) -> ChainIntoIter<Self, B> {
         ChainIntoIter { a: self, b: b }
     }
 }
 
-impl<'t, A, B> IntoIterator for &'t ChainIntoIter<A, B>
-where
+impl<T: IntoIterator + Copy> Chain for T {}
+
+impl<
     A: IntoIterator + Copy,
-    B: IntoIterator<Item = A::Item> + Copy,
-{
+    B: IntoIterator<Item = A::Item> + Copy
+> IntoIterator for &'_ ChainIntoIter<A, B> {
     type Item = A::Item;
     type IntoIter = std::iter::Chain<A::IntoIter, B::IntoIter>;
     fn into_iter(self) -> Self::IntoIter {
@@ -36,50 +32,10 @@ where
     }
 }
 
-impl <A, B> IntoIterator for ChainIntoIter<A, B>
-where
-    A: IntoIterator + Copy,
-    B: IntoIterator<Item = A::Item> + Copy,
-{
-    type Item = A::Item;
-    type IntoIter = std::iter::Chain<A::IntoIter, B::IntoIter>;
-    fn into_iter(self) -> Self::IntoIter {
-        (&self).into_iter()
-    }
-}
+impl<A: IntoIterator + Copy, B: IntoIterator<Item = A::Item> + Copy> Copy for ChainIntoIter<A, B> {}
 
-impl<A, B> Copy for ChainIntoIter<A, B>
-where
-    A: IntoIterator + Copy,
-    B: IntoIterator<Item = A::Item> + Copy,
-{
-}
-
-impl<A, B> Clone for ChainIntoIter<A, B>
-where
-    A: IntoIterator + Copy,
-    B: IntoIterator<Item = A::Item> + Copy,
-{
+impl<A: IntoIterator + Copy, B: IntoIterator<Item = A::Item> + Copy> Clone for ChainIntoIter<A, B> {
     fn clone(&self) -> Self {
         ChainIntoIter { a: self.a, b: self.b }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn copy_test() {
-        let result = [1, 2, 3].chain(&[4, 5, 6]);
-        let x = result.chain(&[7, 8, 9]);
-        let v: Vec<isize> = x.into_iter().map(|x| *x).collect();
-        assert_eq!(vec!(1, 2, 3, 4, 5, 6, 7, 8, 9), v);
-    }
-    #[test]
-    fn ref_test() {
-        let result = [1, 2, 3].chain(&[4, 5, 6]);
-        let x = &result.chain(&[7, 8, 9]);
-        let v: Vec<isize> = x.into_iter().map(|x| *x).collect();
-        assert_eq!(vec!(1, 2, 3, 4, 5, 6, 7, 8, 9), v);
     }
 }
